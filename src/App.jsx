c33f0576b03904cadd5758a7ec4cf48c3e38b8ce
Marc GMO <marc.gmo17@gmail.com>
2023-05-14 14:39:54 -0600

    Lista

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const nombre = "Dervis Rubiel Lara Esteban";
+  return (
+    <div><font color="white">
+      <h1>Lista de Hobbies de Marcos</h1>
+      <ol>
+        <li>Escuchar Música</li>
+        <li>Jugar en Móvil o PC</li>
+        <li>Leer</li>
+        <li>Dibujar</li>
+        <li>Cantar</li>
+        <li>Ver series o anime</li>
+      </ol></font>
+    </div>
+  )
+}
+export default App
